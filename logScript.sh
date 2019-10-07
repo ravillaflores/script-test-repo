c87617c8d3beab209fb6abc20977f06c450d131f
ravillaflores <52301859+ravillaflores@users.noreply.github.com>
2019-10-07 14:41:09 +0800

    Added logging script

---
+++ logScript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo mkdir ~/Desktop/cpusys
+sudo mkdir ~/Desktop/cpusys/Script
+sudo mkdir ~/Desktop/cpusys/Script/Logs
+
+while : 
+do	
+
+	sudo echo \{ \"Time\": `date +%s`\, \"Host\": \"`hostname`\"\,   \"CPU\": `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`\, \"RAM\": `free -m | awk '/Mem:/ { printf($3/$2*100) }'`\, \"HDD\": `df -h / | sed 's/%//' | awk '/\// {print $(NF-1)}'` \} >> ~/Desktop/cpusys/Script/Logs/cpusys.log
+
+	sleep 60
+done
