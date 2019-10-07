9a4b055884a8b86b864e862f3e9b8b02b34df4d4
ravillaflores <52301859+ravillaflores@users.noreply.github.com>
2019-10-07 16:10:42 +0800

    Add files via upload

--- logScript.sh
+++ logScript.sh
@@ -16,16 +16,21 @@ else
 	sudo mkdir /var/log/cpusys-logger/Scripts
 fi
 
+sudo cd  /var/log/cpusys-logger/Scripts
 
-echo -e "while : 
+cat << logScript.sh
+#!/bin/bash
+while : 
 do	
 
 	echo \{ \"Time\": `date +%s`\, \"Host\": \"`hostname`\"\, \"CPU\": `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`\, \"RAM\": `free -m | awk '/Mem:/ { printf($3/$2*100) }'`\, \"HDD\": `df -h / | sed 's/%//' | awk '/\// {print $(NF-1)}'` \} >> sudo /var/log/cpusys-logger/Logs/cpusys.log
 
 	sleep 60
-done " >> sudo /var/log/cpusys-logger/Scripts/logScript.sh
+done
+logScript.sh
 
 
-sudo chmod +x /var/log/cpusys-logger/Scripts/logScript.sh
+
+sudo chmod +x logScript.sh
 
 
