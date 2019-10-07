a791826b5624bbc15d2b6a56ed6501fe006e8896
ravillaflores <52301859+ravillaflores@users.noreply.github.com>
2019-10-07 15:18:46 +0800

    editted Log SCript

--- logScript.sh
+++ logScript.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 
-sudo mkdir ~/Desktop/cpusys
-sudo mkdir ~/Desktop/cpusys/Script
-sudo mkdir ~/Desktop/cpusys/Script/Logs
+
+if [ -d "/var/log/cpusys-logger" ] 
+then
+	if [ -d "/var/log/cpusys-logger/Logs" ] 
+	then
+		echo ""
+	else
+		sudo mkdir /var/log/cpusys-logger/Logs
+	fi
+else
+	sudo mkdir /var/log/cpusys-logger
+	sudo mkdir /var/log/cpusys-logger/Logs
+fi
+
 
 while : 
 do	
 
-	echo \{ \"Time\": `date +%s`\, \"Host\": \"`hostname`\"\,   \"CPU\": `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`\, \"RAM\": `free -m | awk '/Mem:/ { printf($3/$2*100) }'`\, \"HDD\": `df -h / | sed 's/%//' | awk '/\// {print $(NF-1)}'` \} >> /home/rav/Desktop/Logger2/Logs/cpusys.log
+	echo \{ \"Time\": `date +%s`\, \"Host\": \"`hostname`\"\, \"CPU\": `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`\, \"RAM\": `free -m | awk '/Mem:/ { printf($3/$2*100) }'`\, \"HDD\": `df -h / | sed 's/%//' | awk '/\// {print $(NF-1)}'` \} >> /var/log/cpusys-logger/Logs/cpusys.log
 
 	sleep 60
 done
