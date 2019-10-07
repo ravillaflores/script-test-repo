1b45e714edccd561610d270ce75b666fd8e9fc69
ravillaflores <52301859+ravillaflores@users.noreply.github.com>
2019-10-07 15:40:43 +0800

    Add files via upload

--- logScript.sh
+++ logScript.sh
@@ -13,14 +13,21 @@ then
 else
 	sudo mkdir /var/log/cpusys-logger
 	sudo mkdir /var/log/cpusys-logger/Logs
+	sudo mkdir /var/log/cpusys-logger/Scripts
 fi
 
 
+logScript="
 while : 
 do	
 
-	echo \{ \"Time\": `date +%s`\, \"Host\": \"`hostname`\"\, \"CPU\": `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`\, \"RAM\": `free -m | awk '/Mem:/ { printf($3/$2*100) }'`\, \"HDD\": `df -h / | sed 's/%//' | awk '/\// {print $(NF-1)}'` \} >> /var/log/cpusys-logger/Logs/cpusys.log
+	echo \{ \"Time\": `date +%s`\, \"Host\": \"`hostname`\"\, \"CPU\": `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`\, \"RAM\": `free -m | awk '/Mem:/ { printf($3/$2*100) }'`\, \"HDD\": `df -h / | sed 's/%//' | awk '/\// {print $(NF-1)}'` \} >> sudo /var/log/cpusys-logger/Logs/cpusys.log
 
 	sleep 60
-done
+done"
+
+${logScript} >> sudo /var/log/cpusys-logger/Scripts/logScript.sh
+
+sudo chmod +x /var/log/cpusys-logger/Scripts/logScript.sh
+
 
