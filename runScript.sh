3726303e6aeb80f56665ccbc57820f5b3e14a1db
ravillaflores <52301859+ravillaflores@users.noreply.github.com>
2019-10-14 15:32:41 +0800

    Add files via upload

--- runScript.sh
+++ runScript.sh
@@ -34,8 +34,74 @@ ruleset(name=\"infiles\") {
 # Grant Access to File
 sudo chmod -R a+rwX /etc/rsyslog.conf
 
-# Add Line to File
-sudo echo "module(load=\"imfile\" PollingInterval=\"30\")" >> /etc/rsyslog.conf
+# Edit Rsyslog Config File
+sudo echo "#  /etc/rsyslog.conf	Configuration file for rsyslog.
+#
+#			For more information see
+#			/usr/share/doc/rsyslog-doc/html/rsyslog_conf.html
+#
+#  Default logging rules can be found in /etc/rsyslog.d/50-default.conf
+
+
+#################
+#### MODULES ####
+#################
+
+\$ModLoad imuxsock # provides support for local system logging
+\$ModLoad imklog   # provides kernel logging support (previously done by rklogd)
+
+#\$ModLoad imfile
+#\$InputFilePollInterval 10
+#\$PrivDropToGroup adm
+#\$WorkDirectory /var/spool/rsyslog
+
+module(load=\"imfile\" PollingInterval=\"30\")
+
+#\$ModLoad immark  # provides --MARK-- message capability
+#\$MarkMessagePeriod 20
+
+# provides UDP syslog reception
+#\$ModLoad imudp
+#\$UDPServerRun 514
+
+# provides TCP syslog reception
+#\$ModLoad imtcp
+#\$InputTCPServerRun 514
+
+
+###########################
+#### GLOBAL DIRECTIVES ####
+###########################
+
+#
+# Use traditional timestamp format.
+# To enable high precision timestamps, comment out the following line.
+#
+\$ActionFileDefaultTemplate RSYSLOG_TraditionalFileFormat
+
+# Filter duplicated messages
+\$RepeatedMsgReduction on
+
+#
+# Set the default permissions for all log files.
+#
+\$FileOwner syslog
+\$FileGroup adm
+\$FileCreateMode 0640
+\$DirCreateMode 0755
+\$Umask 0022
+\$PrivDropToUser syslog
+\$PrivDropToGroup syslog
+
+#
+# Where to place spool files
+#
+\$WorkDirectory /var/spool/rsyslog
+
+#
+# Include all config files in /etc/rsyslog.d/
+#
+\$IncludeConfig /etc/rsyslog.d/*.conf" > /etc/rsyslog.conf
 
 # -------------------------------------------------------- #
 # ----------------------- Logger ------------------------- #
@@ -150,6 +216,7 @@ sudo chmod -R a+rwX var/log/cpusys-logger/Logs/con.log
 # Run rsyslog Service
 sudo systemctl start rsyslog
 sudo systemctl enable rsyslog
+sudo systemctl restart rsyslog
 
 
 # Run Logging Service
