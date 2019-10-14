9f50adce9f62b943b1af6eed7ec3d3491e9cce85
ravillaflores <52301859+ravillaflores@users.noreply.github.com>
2019-10-14 15:23:04 +0800

    Add files via upload

--- regionHostLog.sh
+++ regionHostLog.sh
@@ -10,8 +10,8 @@ sudo echo "Begin Installation..."
 
 sudo echo "Installing Dependecies..."
 #sudo apt-get install npm
-sudo apt-get remove -y rsyslog
-sudo apt-get purge -y rsyslog
+#sudo apt-get remove -y rsyslog
+#sudo apt-get purge -y rsyslog
 sudo add-apt-repository -y ppa:adiscon/v8-devel
 sudo apt-get update -y
 sudo apt-get install -y rsyslog
